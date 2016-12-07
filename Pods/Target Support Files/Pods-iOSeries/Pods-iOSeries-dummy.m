90799bf86108a96ae7ae48709af8216a5cbb880f
Pierre <pierre.boudon.27@gmail.com>
2016-12-07 14:04:26 +0100

    Initial commit

---
+++ Pods/Target Support Files/Pods-iOSeries/Pods-iOSeries-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSeries : NSObject
+@end
+@implementation PodsDummy_Pods_iOSeries
+@end
---
+++ iOSeries/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSeries
+//
+//  Created by Pierre on 07/12/2016.
+//  Copyright © 2016 Pierre Boudon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
